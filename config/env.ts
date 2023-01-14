117af5c54d1e11ee366cdaf374223eff27ba3046
David Azemoh Paul <david@thriveagric.com>
2023-01-14 16:22:10 +0100

    RAILWAY_ENVIRONMENT-check-PORT

--- config/env.ts
+++ config/env.ts
@@ -1,15 +1,7 @@
 import dotenv from 'dotenv';
 
-console.log('No value for FOO yet:', process.env.FOO);
-console.log('ENV IS:', process.env);
-console.log('NODE EnV is:', process.env.NODE_ENV);
-console.log('RAILWAY_ENVIRONMENT EnV is:', process.env.RAILWAY_ENVIRONMENT);
-console.log('PORT EnV is:', process.env.PORT);
-
 if (process.env.RAILWAY_ENVIRONMENT !== 'production') {
   const dotenvResult = dotenv.config();
-  console.log('========================xxxx', dotenvResult);
-
   // if (dotenvResult.error) {
   //   throw dotenvResult.error;
   // }
