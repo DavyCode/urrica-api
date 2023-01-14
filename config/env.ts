5ed49ef6e01efecf94311410b18719e6cd8fb6f1
David Azemoh Paul <david@thriveagric.com>
2023-01-14 15:29:37 +0100

    use .env in dev only

--- config/env.ts
+++ config/env.ts
@@ -1,11 +1,24 @@
 import dotenv from 'dotenv';
 
-const dotenvResult = dotenv.config();
-console.log('========================xxxx', dotenvResult);
+// const dotenvResult = dotenv.config();
+// console.log('========================xxxx', dotenvResult);
 
-if (dotenvResult.error) {
-  throw dotenvResult.error;
+// if (dotenvResult.error) {
+//   throw dotenvResult.error;
+// }
+
+console.log('No value for FOO yet:', process.env.FOO);
+
+if (process.env.NODE_ENV !== 'production') {
+  const dotenvResult = dotenv.config();
+  console.log('========================xxxx', dotenvResult);
+
+  if (dotenvResult.error) {
+    throw dotenvResult.error;
+  }
 }
+
+console.log('Now the value for FOO is:', process.env.FOO);
 
 export const { PORT, JWT_SECRET, JWT_EXPIRATION_MINUTES, DBURL, JWT_BEARER } =
   process.env;
