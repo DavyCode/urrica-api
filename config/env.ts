7f8c77e34aa9a5b8c8778cc382916acb252ac93f
David Azemoh Paul <david@thriveagric.com>
2023-01-14 15:45:40 +0100

    log process

--- config/env.ts
+++ config/env.ts
@@ -1,13 +1,8 @@
 import dotenv from 'dotenv';
 
-// const dotenvResult = dotenv.config();
-// console.log('========================xxxx', dotenvResult);
-
-// if (dotenvResult.error) {
-//   throw dotenvResult.error;
-// }
-
 console.log('No value for FOO yet:', process.env.FOO);
+console.log('ENV IS:', process.env);
+console.log('NODE EnV is:', process.env.NODE_ENV);
 
 if (process.env.NODE_ENV !== 'production') {
   const dotenvResult = dotenv.config();
@@ -17,8 +12,6 @@ if (process.env.NODE_ENV !== 'production') {
     throw dotenvResult.error;
   }
 }
-
-console.log('Now the value for FOO is:', process.env.FOO);
 
 export const { PORT, JWT_SECRET, JWT_EXPIRATION_MINUTES, DBURL, JWT_BEARER } =
   process.env;
