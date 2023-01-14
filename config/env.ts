f5b5e1ceaf8b31c8380d027d8236aedf3e749c17
David Azemoh Paul <david@thriveagric.com>
2023-01-14 15:58:22 +0100

    RAILWAY_ENVIRONMENT-1

--- config/env.ts
+++ config/env.ts
@@ -3,17 +3,15 @@ import dotenv from 'dotenv';
 console.log('No value for FOO yet:', process.env.FOO);
 console.log('ENV IS:', process.env);
 console.log('NODE EnV is:', process.env.NODE_ENV);
+console.log('RAILWAY_ENVIRONMENT EnV is:', process.env.RAILWAY_ENVIRONMENT);
 
-if (
-  process.env.RAILWAY_ENVIRONMENT !== 'production' ||
-  process.env.NODE_ENV !== 'production'
-) {
+if (process.env.RAILWAY_ENVIRONMENT !== 'production') {
   const dotenvResult = dotenv.config();
   console.log('========================xxxx', dotenvResult);
 
-  if (dotenvResult.error) {
-    throw dotenvResult.error;
-  }
+  // if (dotenvResult.error) {
+  //   throw dotenvResult.error;
+  // }
 }
 
 export const { PORT, JWT_SECRET, JWT_EXPIRATION_MINUTES, DBURL, JWT_BEARER } =
